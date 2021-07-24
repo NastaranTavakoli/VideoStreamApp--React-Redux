c90a0b2054a8da45ebcfc2109e25940e81eaa749
nastaran-tavakoli <nastaran.tavakoli.91@gmail.com>
2021-07-24 12:35:27 +1000

    Update StreamList component

--- Client/src/pages/StreamList.js
+++ Client/src/pages/StreamList.js
@@ -1,6 +1,66 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchStreams } from "../actions";
+import { Link } from "react-router-dom";
 
 export const StreamList = () => {
-  return <div>StreamList</div>;
-};
+  const dispatch = useDispatch();
+  const streams = useSelector(state => state.streams);
+  const auth = useSelector(state => state.auth);
 
+  useEffect(() => {
+    dispatch(fetchStreams());
+  }, []);
+
+  const renderCreateButton = () => {
+    if (auth.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new">
+            <button className="ui button primary">Create Stream</button>
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  const renderAdminButtons = stream => {
+    if (auth.userId === stream) {
+      return (
+        <div className="right floated content">
+          <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>
+            EDIT
+          </Link>
+          <Link
+            className="ui button negative"
+            to={`/streams/delete/${stream.id}`}
+          >
+            DELETE
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  const renderList = () =>
+    Object.values(streams).map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {renderAdminButtons(stream)}
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            <div className="header">{stream.title}</div>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+
+  return (
+    <div>
+      <div>{renderList(streams)}</div>
+      <div>{renderCreateButton()}</div>
+    </div>
+  );
+};
--- Client/src/reducers/streamReducer.js
+++ Client/src/reducers/streamReducer.js
@@ -1,4 +1,4 @@
-import _ from "./lodash";
+import _ from "lodash";
 import {
   FETCH_STREAM,
   FETCH_STREAMS,
